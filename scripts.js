c5795b976761230988be603c174a76e5ad04e1e9
Jeni <lajennif@gmail.com>
2018-12-05 22:50:16 -0500

    attempt at committing 12/05/18

--- scripts.js
+++ scripts.js
@@ -6,9 +6,49 @@ let anchorLinks = document.querySelectorAll('a[href^="#"]');
 console.log(anchorLinks);
 
 for (let item of anchorLinks) {
-  console.log(item);
   item.addEventListener('click', (e)=> {
+    alert("You are navigating to my " + item.innerHTML + " section!");
     console.log("this is what e is: ", e); 
     console.log('this is what i clicked', item);
   })
+}
+
+// Slide function
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
+// Scrolling function
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
